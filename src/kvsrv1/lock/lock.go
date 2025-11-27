edf01c94dcbea8227dca20d58ae04ff1f3d3c70e
Sean Lin <yuanmu2004@163.com>
2025-11-27 22:49:47 +0800

    feat: 完成基于kvserver的锁服务，通过测试

--- src/kvsrv1/lock/lock.go
+++ src/kvsrv1/lock/lock.go
@@ -1,6 +1,11 @@
 package lock
 
 import (
+	// "6.5840/kvsrv1/rpc"
+	"math/rand"
+	"time"
+
+	"6.5840/kvsrv1/rpc"
 	"6.5840/kvtest1"
 )
 
@@ -11,6 +16,9 @@ type Lock struct {
 	// MakeLock().
 	ck kvtest.IKVClerk
 	// You may add code here
+	l string
+	// version rpc.Tversion
+	id string
 }
 
 // The tester calls MakeLock() and passes in a k/v clerk; your code can
@@ -19,15 +27,48 @@ type Lock struct {
 // Use l as the key to store the "lock state" (you would have to decide
 // precisely what the lock state is).
 func MakeLock(ck kvtest.IKVClerk, l string) *Lock {
-	lk := &Lock{ck: ck}
+	lk := &Lock{ck: ck, l: l, id: "lock"}
 	// You may add code here
 	return lk
 }
 
 func (lk *Lock) Acquire() {
 	// Your code here
+	for {
+		value, version, err := lk.ck.Get(lk.l)
+		if err == rpc.ErrNoKey {
+			err = lk.ck.Put(lk.l, lk.id, 0)
+			if err == rpc.OK {
+				return
+			}
+		} else {
+			// lock is free
+			if value == "" {
+				err = lk.ck.Put(lk.l, lk.id, version)
+				if err == rpc.OK {
+					return
+				}
+			} 
+		}
+		time.Sleep(time.Duration(rand.Intn(60) + 20) * time.Millisecond)
+	}
+	
 }
 
 func (lk *Lock) Release() {
 	// Your code here
+	for {
+		value, version, err := lk.ck.Get(lk.l)
+		if err == rpc.ErrNoKey {
+			return
+		} else {
+			if value == lk.id {
+				err = lk.ck.Put(lk.l, "", version)
+				if err == rpc.OK {
+					return
+				}
+			}
+		}
+		time.Sleep(time.Duration(rand.Intn(60) + 20) * time.Millisecond)
+	}
 }
